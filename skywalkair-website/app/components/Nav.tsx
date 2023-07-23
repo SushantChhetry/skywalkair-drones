a9dee41722c7fc6a8895d1d3425c0df4b5bdf00d
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-23 14:41:21 -0500

    Added Nav and home css

---
+++ skywalkair-website/app/components/Nav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="flex items-center justify-between px-2 py-2">
+        <button className="hover:text-gray-300 py-2 px-4 rounded-2xl text-blue-700 text-2xl">
+          <GiHamburgerMenu />
+        </button>
+
+        <button className="bg-indigo-s600 hover:bg-indigo-700 text-white py-2 px-4 rounded-2xl">
+          Contact Us!
+        </button>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
