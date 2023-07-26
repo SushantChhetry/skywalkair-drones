ecabacbc19ec57312844c8f5dd25f818cb2eff72
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-25 21:56:59 -0500

    Added down arrow

--- skywalkair-website/app/services/page.tsx
+++ skywalkair-website/app/services/page.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { BsArrowDownCircle } from "react-icons/bs";
 
 const Service = () => {
   return (
     <div className=" h-screen flex flex-col justify-center items-center px-5">
       <h1 className=" text-indigo-700 text-4xl">Our Services</h1>
-      <div className="grid grid-cols-3 gap-4 max-w-screen-xl w-screen my-14 h-4/6">
-        <div className=" p-4 bg-indigo-100 hover:bg-indigo-400">Row 1</div>
-        <div className="p-4 bg-indigo-200 hover:bg-indigo-400">Row 2</div>
-        <div className="p-4 bg-indigo-300 hover:bg-indigo-400">Row 3</div>
+      <div className="grid grid-cols-3 max-w-screen-xl w-screen my-14 h-4/6">
+        <div className=" p-4 bg-indigo-100 ">
+          Row 1{" "}
+          <BsArrowDownCircle
+            size={40}
+            className=" text-indigo-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-indigo-300 duration-300"
+          />
+        </div>
+        <div className="p-4 bg-indigo-200">
+          Row 2{" "}
+          <BsArrowDownCircle
+            size={40}
+            className=" text-indigo-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-indigo-300 duration-300"
+          />
+        </div>
+        <div className="p-4 bg-indigo-300 ">
+          Row 3{" "}
+          <BsArrowDownCircle
+            size={40}
+            className=" text-indigo-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-indigo-500 duration-300"
+          />
+        </div>
       </div>
     </div>
   );
