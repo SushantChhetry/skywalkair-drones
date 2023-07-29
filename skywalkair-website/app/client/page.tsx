a21c1f48e44a9f31d0418a672766e6f69939c522
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-07-29 10:07:39 -0500

    Basic testimonials

--- skywalkair-website/app/client/page.tsx
+++ skywalkair-website/app/client/page.tsx
@@ -12,6 +12,43 @@ const Client = () => {
         <AiFillStar size={32} />
         <AiFillStar size={32} />
       </div>
+      <div className="grid grid-cols-2 grid-rows-3 gap-3">
+        <div className="p-4 text-indigo-700 font-semibold">
+          <h1>Javier Burrows</h1>
+          <h4>Real Estate</h4>
+        </div>
+
+        <div className="bg-gray-200 p-4 ">
+          <p>
+            Skywalkaik&apos;s breathtaking drone footage brought our real estate
+            listings to life, impressing clients and increasing our sales
+          </p>
+        </div>
+
+        <div className=" p-4 text-indigo-700 font-semibold">
+          <h1>Javier Burrows</h1>
+          <h4>Real Estate</h4>
+        </div>
+
+        <div className="bg-gray-200 p-4">
+          <p>
+            Skywalkaik&apos;s breathtaking drone footage brought our real estate
+            listings to life, impressing clients and increasing our sales
+          </p>
+        </div>
+
+        <div className=" p-4 text-indigo-700 font-semibold">
+          <h1>Javier Burrows</h1>
+          <h4>Real Estate</h4>
+        </div>
+
+        <div className="bg-gray-200 p-4">
+          <p>
+            Skywalkaik&apos;s breathtaking drone footage brought our real estate
+            listings to life, impressing clients and increasing our sales
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
