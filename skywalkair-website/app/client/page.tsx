7ac1c31c65f00712a9fbf4b3a91a61fb9403e9a8
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-08-01 21:30:54 -0500

    Fixed Testimonial Page

--- skywalkair-website/app/client/page.tsx
+++ skywalkair-website/app/client/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import Link from "next/link";
 
 const Client = () => {
   return (
@@ -12,37 +13,43 @@ const Client = () => {
         <AiFillStar size={32} />
         <AiFillStar size={32} />
       </div>
-      <div className="grid grid-cols-2 grid-rows-3 gap-3">
-        <div className="p-4 text-indigo-700 font-semibold">
-          <h1>Javier Burrows</h1>
-          <h4>Real Estate</h4>
+      <div className="grid grid-cols-2 grid-rows-3  max-w-4xl ">
+        <div className="p-4 text-indigo-700 flex flex-col justify-center items-center">
+          <Link href="#" target="_blank">
+            <h1 className="font-semibold">Javier Burrows</h1>
+            <p>Real Estate Inc</p>
+          </Link>
         </div>
 
-        <div className="bg-gray-200 p-4 ">
+        <div className="bg-gray-200 p-4 my-4">
           <p>
             Skywalkaik&apos;s breathtaking drone footage brought our real estate
             listings to life, impressing clients and increasing our sales
           </p>
         </div>
 
-        <div className=" p-4 text-indigo-700 font-semibold">
-          <h1>Javier Burrows</h1>
-          <h4>Real Estate</h4>
+        <div className="p-4 text-indigo-700 flex flex-col justify-center items-center">
+          <Link href="#" target="_blank">
+            <h1 className="font-semibold">Javier Burrows</h1>
+            <p>Real Estate Inc</p>
+          </Link>
         </div>
 
-        <div className="bg-gray-200 p-4">
+        <div className="bg-gray-200 p-4  my-4">
           <p>
             Skywalkaik&apos;s breathtaking drone footage brought our real estate
             listings to life, impressing clients and increasing our sales
           </p>
         </div>
 
-        <div className=" p-4 text-indigo-700 font-semibold">
-          <h1>Javier Burrows</h1>
-          <h4>Real Estate</h4>
+        <div className="p-4 text-indigo-700 flex flex-col justify-center items-center">
+          <Link href="#" target="_blank">
+            <h1 className="font-semibold">Javier Burrows</h1>
+            <p>Real Estate Inc</p>
+          </Link>
         </div>
 
-        <div className="bg-gray-200 p-4">
+        <div className="bg-gray-200 p-4  my-4">
           <p>
             Skywalkaik&apos;s breathtaking drone footage brought our real estate
             listings to life, impressing clients and increasing our sales
