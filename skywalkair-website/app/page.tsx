eb21a0dd1436b19be7adb6d74e214b63f91db66a
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-08-06 14:19:52 -0500

    Added placeholder logo

--- skywalkair-website/app/page.tsx
+++ skywalkair-website/app/page.tsx
@@ -18,17 +18,13 @@ export default function Home() {
       <Nav />
       <main className=" bg-blue-50 flex flex-col justify-center items-center  h-screen">
         <div className="flex px-5 py-2 m-10 items-center justify-between  gap-10">
-          <div className="max-w-lg flex justify-center items-center flex-col">
-            <h2 className=" text-4xl text-blue-900">
+          <div className="max-w-2xl flex justify-center items-center flex-col">
+            <h2 className=" text-5xl text-blue-900">
               Enhance your visuals with awe-inspiring aerial perspectives filmed
               by our expert drone videography services.
             </h2>
-            <br />
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-2xl">
-              Start Now
-            </button>
           </div>
-          <p>Logo</p>
+          <Image src="/logo.png" width={350} height={350} alt="logo" />
         </div>
         <Link href="#about">
           <BsArrowDownCircle
