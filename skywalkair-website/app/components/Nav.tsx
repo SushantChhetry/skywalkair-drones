fc9a26e97d2f32c9962faacae681853dcd1f1819
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-08-06 14:05:11 -0500

    Added react smooth scrolling to Nav

--- skywalkair-website/app/components/Nav.tsx
+++ skywalkair-website/app/components/Nav.tsx
@@ -2,7 +2,7 @@
 
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState, useEffect } from "react";
-import Link from "next/link";
+import { Link } from "react-scroll";
 
 const Nav = () => {
   const [navOpen, setNavOpen] = useState(false);
@@ -20,28 +20,75 @@ const Nav = () => {
         >
           <GiHamburgerMenu />
         </button>
-
-        <button className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-2xl">
-          Contact Us!
-        </button>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          duration={500}
+        >
+          <button className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-2xl">
+            Contact Us!
+          </button>
+        </Link>
       </nav>
       {navOpen ? (
         <div className=" max-w-screen bg-indigo-600 text-white p-4 h-40">
           <ul>
             <li className="hover:text-gray-400  w-fit">
-              <Link href="#contact">Contact Us</Link>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Contact Us
+              </Link>
             </li>
             <li className="hover:text-gray-400 w-fit">
-              <Link href="#about">About</Link>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                About
+              </Link>
             </li>
             <li className="hover:text-gray-400 w-fit">
-              <Link href="#services">Services</Link>
+              <Link
+                activeClass="active"
+                to="services"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Services
+              </Link>
             </li>
             <li className="hover:text-gray-400 w-fit">
-              <Link href="#client">Testimonials</Link>
+              <Link
+                activeClass="active"
+                to="client"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Testimonials
+              </Link>
             </li>
             <li className="hover:text-gray-400 w-fit">
-              <Link href="#ceo">CEO</Link>
+              <Link
+                activeClass="active"
+                to="ceo"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                CEO
+              </Link>
             </li>
           </ul>
         </div>
--- skywalkair-website/app/social/pages.tsx
+++ skywalkair-website/app/social/pages.tsx
@@ -13,50 +13,52 @@ const Social = () => {
     instagram: "https://www.google.com/",
     youtube: "https://www.google.com/",
     phone: "2562253874",
-    email:"email@emai.com",
+    email: "email@emai.com",
   };
   return (
-    <div className=" bg-blue-50 flex flex-col justify-center items-center  h-screen max-h-screen ">
-      <h1 className=" text-4xl text-blue-900 my-3">Let&apos;s Connect!</h1>
-      <br />
-      <div className=" grid grid-rows-1 grid-cols-2">
-        <div className="flex flex-col justify-center items-center">
-          <p className=" text-indigo-700 text-lg">Mailing Address</p>
-          <h3 className=" text-indigo-700 text-2xl">
-            9025 Sentinel Cir, Montgomery, AL
-          </h3>
+    <div className="bg-white flex flex-col justify-center items-center  h-screen max-h-screen ">
+      <div className=" bg-blue-50 flex flex-col justify-center items-center w-screen h-5/6 rounded-full max-w-5xl">
+        <h1 className=" text-4xl text-blue-900 my-3">Let&apos;s Connect!</h1>
+        <br />
+        <div className=" grid grid-rows-1 grid-cols-2">
+          <div className="flex flex-col justify-center items-center">
+            <p className=" text-indigo-700 text-lg">Mailing Address</p>
+            <h3 className=" text-indigo-700 text-2xl">
+              9025 Sentinel Cir, Montgomery, AL
+            </h3>
 
-          <br />
+            <br />
 
-          <p className=" text-indigo-700 text-lg">Email</p>
-          <h2 className=" text-indigo-700 text-2xl">email@email.com</h2>
+            <p className=" text-indigo-700 text-lg">Email</p>
+            <h2 className=" text-indigo-700 text-2xl">email@email.com</h2>
 
-          <br />
+            <br />
 
-          <p className=" text-indigo-700 text-lg">Phone Number</p>
-          <Link href={`tel: ${urls.phone}`}>
-            <h3 className=" text-indigo-700 text-2xl">2222222222</h3>
-          </Link>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <Link href={urls.facebook} target="_blank">
-            <AiFillFacebook
-              className=" text-indigo-600 hover:text-indigo-300 my-2"
-              size={50}
-            />
-          </Link>
-          <Link href={urls.facebook} target="_blank">
-            <AiFillTwitterSquare
-              className=" text-indigo-600 hover:text-indigo-300 my-2"
-              size={50}
-            />
-          </Link>
-          <Link href={urls.facebook} target="_blank">
-            <AiFillInstagram
-              className=" text-indigo-600 hover:text-indigo-300 my-2"
-              size={50}
-            />
-          </Link>
+            <p className=" text-indigo-700 text-lg">Phone Number</p>
+            <Link href={`tel: ${urls.phone}`}>
+              <h3 className=" text-indigo-700 text-2xl">2222222222</h3>
+            </Link>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <Link href={urls.facebook} target="_blank">
+              <AiFillFacebook
+                className=" text-indigo-600 hover:text-indigo-300 my-2"
+                size={50}
+              />
+            </Link>
+            <Link href={urls.facebook} target="_blank">
+              <AiFillTwitterSquare
+                className=" text-indigo-600 hover:text-indigo-300 my-2"
+                size={50}
+              />
+            </Link>
+            <Link href={urls.facebook} target="_blank">
+              <AiFillInstagram
+                className=" text-indigo-600 hover:text-indigo-300 my-2"
+                size={50}
+              />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
