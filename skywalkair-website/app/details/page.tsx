8137d629be31331330c34ebebb756386ce553c82
SushantChhetry <60019004+SushantChhetry@users.noreply.github.com>
2023-08-21 22:07:43 -0500

    Added image to ceo bio

--- skywalkair-website/app/details/page.tsx
+++ skywalkair-website/app/details/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 const Detail = () => {
   return (
-    <div className=" h-screen bg-blue-50 flex flex-col justify-center items-center">
+    <div className=" h-screen bg-blue-50 flex flex-col justify-center items-center rounded-full m-5">
       <h1 className=" text-4xl text-blue-900 my-3">Why Us?</h1>
-      Detail wotj ceo
-      <div></div>
-      <div></div>
+      <div className=" grid grid-cols-2 grid-rows-1 m-5">
+        <div>
+          <Image src="/WhyUs.jpg" alt="drone" width={500} height={500} />
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
